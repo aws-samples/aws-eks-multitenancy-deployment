7f2f985062510ec9eed9f8ffbd48454f55edb714
ambatiap <ambatiap@amazon.com>
2023-10-27 18:19:31 +0530

    updated script and gitignore

--- run.sh
+++ run.sh
@@ -1,72 +1,42 @@
 #!/usr/bin/env bash
 
-################################################################################
 # Variables
-################################################################################
-
 RED='\033[0;31m'
 BACKEND_BUCKET_ID="container-focus-group-tfstate"
 DYNAMODB_ID="terraform-locks"
 
-################################################################################
 # Functions
-################################################################################
-
 function usage {
-    echo "Usage: $(basename $0) [-app|env|region|tfcmd]" 2>&1
-    echo ' -module      | -m        module to deploy eg: network'
-    echo ' -env         | -e        environment to deploy eg: demo'
-    echo ' -region      | -r        AWS region to deploy eg: us-west-2'
-    echo ' -tfcmd       | -t        terraform command to run eg: init|plan|apply'
+    echo "Usage: $(basename "$0") [-module|-m] [-env|-e] [-region|-r] [-tfcmd|-t]" 2>&1
+    echo ' -module | -m  module to deploy eg: network'
+    echo ' -env    | -e  environment to deploy eg: demo'
+    echo ' -region | -r  AWS region to deploy eg: us-west-2'
+    echo ' -tfcmd  | -t  terraform command to run eg: init|plan|apply'
     exit 1
 }
 
-################################################################################
-# Command Line Tool
-################################################################################
-
-if [[ ${#} -eq 0 ]]; then
+# Check for command-line arguments
+if [[ $# -eq 0 ]]; then
     usage
 fi
 
+# Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
     -module | -m)
         TF_MODULE=$2
         shift 2
-        if [[ "$1" == "" || $2 = "" ]]; then
-            echo -e "${RED}Error: Missing argument\n"
-            usage
-            break
-        fi
         ;;
     -env | -e)
         TF_ENV=$2
         shift 2
-        if [[ "$1" == "" || $2 = "" ]]; then
-            echo -e "${RED}Error: Missing argument\n"
-            usage
-            break
-        fi
         ;;
-
     -region | -r)
         TF_REGION=$2
         shift 2
-        if [[ "$1" == "" || $2 = "" ]]; then
-            echo -e "${RED}Error: Missing argument\n"
-            usage
-            break
-        fi
         ;;
-
     -tfcmd | -t)
         TF_CMD=$2
-        if [[ "$1" == "" || $2 = "" ]]; then
-            echo -e "${RED}Error: Missing argument\n"
-            usage
-            break
-        fi
         shift 2
         ;;
     *)
@@ -75,42 +45,41 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
+# Validate and set directories
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TF_VARS_PATH="$DIR/$TF_ENV"
 TF_EXECUTION_DIR="$DIR/$TF_ENV/$TF_MODULE"
 
 # Always run from the location of this script
-cd $DIR
+cd "$DIR" || exit 1
 
-if [ ! -d "$TF_EXECUTION_DIR" ]; then
-    echo >&2 "Directory $TF_EXECUTION_DIR does not exist"
+# Check if directories exist
+if [ ! -d "$TF_EXECUTION_DIR" ] || [ ! -d "$TF_VARS_PATH" ]; then
+    echo >&2 "Error: Directories $TF_EXECUTION_DIR or $TF_VARS_PATH do not exist"
     usage
-    exit 1
-fi
-
-if [ ! -d "$TF_VARS_PATH" ]; then
-    echo >&2 "Directory $TF_VARS_PATH does not exist"
-    usage
-    exit 1
 fi
 
 # Set provider region and environment variable
-export AWS_REGION=$TF_REGION
-export TF_VAR_environment=$TF_ENV
+export AWS_REGION="$TF_REGION"
+export TF_VAR_environment="$TF_ENV"
 
+# Handle Terraform commands
 if [ "$TF_CMD" == "init" ]; then
-    terraform -chdir=$TF_EXECUTION_DIR init \
-        -backend-config="bucket="${BACKEND_BUCKET_ID}"" \
+    terraform -chdir="$TF_EXECUTION_DIR" init \
+        -backend-config="bucket=${BACKEND_BUCKET_ID}" \
         -backend-config="key=$TF_ENV/$TF_REGION/$TF_MODULE/terraform.tfstate" \
-        -backend-config="dynamodb_table="${DYNAMODB_ID}"" \
+        -backend-config="dynamodb_table=${DYNAMODB_ID}" \
         -backend-config="region=$TF_REGION"
 elif [ "$TF_CMD" == "plan" ]; then
-    terraform -chdir=$TF_EXECUTION_DIR $TF_CMD -out tfapply
+    if [ "$TF_MODULE" == "pipeline" ]; then
+        terraform -chdir="$TF_EXECUTION_DIR" "$TF_CMD"
+    else
+        terraform -chdir="$TF_EXECUTION_DIR" "$TF_CMD" -out tfapply
+    fi
 elif [ "$TF_CMD" == "apply" ]; then
-    terraform -chdir=$TF_EXECUTION_DIR $TF_CMD -input=false tfapply
+    terraform -chdir="$TF_EXECUTION_DIR" "$TF_CMD" -input=false tfapply
 elif [ "$TF_CMD" == "destroy" ]; then
-    terraform -chdir=$TF_EXECUTION_DIR $TF_CMD --auto-approve
+    terraform -chdir="$TF_EXECUTION_DIR" "$TF_CMD" --auto-approve
 else
-    terraform -chdir=$TF_EXECUTION_DIR $TF_CMD
+    terraform -chdir="$TF_EXECUTION_DIR" "$TF_CMD"
 fi
